63f13b9a21829f1391b310d35d6c9186a9a33031
Connie Leung <cleung334@gmail.com>
2020-10-13 21:46:40 +0800

    chore: install dependencies for apollo-server and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import 'reflect-metadata'
+
+import { ApolloServer } from 'apollo-server-express'
+
+console.log('ApolloServer', ApolloServer)
+
 console.log('hello world')
 console.log('Good morning')
